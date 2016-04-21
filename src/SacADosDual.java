1bc43f9289928d045c038f433ed76b85fabcab6f
yannBlanchard <yann.77.blanchard@gmail.com>
2016-04-21 22:10:42 +0200

    Test

--- src/SacADosDual.java
+++ src/SacADosDual.java
@@ -13,4 +13,22 @@ public class SacADosDual {
     public void showSolution(){
 
     }
+
+    public int getValue(int v1,int v2){
+
+        return Math.min();
+    }
+
+    public void premiereColonne(int utiliteI,int e){
+        for(int i =0;i<2;i++){
+
+        }
+    }
+
+    public void calculColonne(int nbColonne){
+        for(int k = nbColonne -1;k <1;k--){
+
+        }
+    }
+
 }
